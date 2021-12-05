47e4dc6bba0a724e024bc738865dd41e33f2fc32
Kelly Vieira <kellyvieira09@hotmail.com>
2021-12-05 14:25:17 -0300

    atividades s2

---
+++ exercicio s2.js
@@ -0,0 +1,15 @@
+let  mensagem  =  ' SEJA BEM VINDO AO MEU SITE' ;
+alerta ( mensagem )
+
+deixe  num1  =  87 ;
+deixe  num2  =  96 ;
+deixe  soma  =  num1 + num2 ;
+alerta ( soma ) ;
+
+deixe  num3  =  5 ;
+deixe  num4  =  8 ;
+deixe  multiplicacao  =  num3 * num4 ;
+alerta ( multiplicação ) ;
+
+let  lista  =  [ 'laranja' ,  'tomate' ,  'uva' ,  'alface' ,  'carne' ] ;
+alerta ( lista ) ;
